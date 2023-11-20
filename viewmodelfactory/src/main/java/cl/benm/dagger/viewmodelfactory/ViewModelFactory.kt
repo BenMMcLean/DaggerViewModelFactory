11f3a4361971718334ad46a375f57e12c8a65a7e
Ben McLean <benmclean007@gmail.com>
2023-11-20 19:04:36 +1100

    Create ViewModelFactory for Dagger injection
    - Add imports for necessary classes
    - Create DaggerViewModelFactory class that extends AbstractSavedStateViewModelFactory
    - Implement create() method to invoke the provided create lambda function
    - Add extension functions daggerViewModels() for ComponentActivity and Fragment classes, which use DaggerViewModelFactory to create ViewModels

--- viewmodelfactory/src/main/java/cl/benm/dagger/viewmodelfactory/ViewModelFactory.kt
+++ viewmodelfactory/src/main/java/cl/benm/dagger/viewmodelfactory/ViewModelFactory.kt
@@ -1,4 +1,40 @@
 package cl.benm.dagger.viewmodelfactory
 
-class ViewModelFactory {
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.savedstate.SavedStateRegistryOwner
+
+class DaggerViewModelFactory<T: ViewModel>(
+    savedStateRegistryOwner: SavedStateRegistryOwner,
+    private val create: (stateHandle: SavedStateHandle) -> T
+) : AbstractSavedStateViewModelFactory(savedStateRegistryOwner, null) {
+
+    override fun <T : ViewModel> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
+        return create.invoke(handle) as T
+    }
+}
+
+inline fun <reified T : ViewModel> ComponentActivity.daggerViewModels(
+    noinline create: (stateHandle: SavedStateHandle) -> T
+) = viewModels<T> {
+    DaggerViewModelFactory(this, create)
+}
+
+inline fun <reified T : ViewModel> Fragment.daggerViewModels(
+    noinline ownerProducer: () -> ViewModelStoreOwner = { this },
+    noinline extrasProducer: (() -> CreationExtras)? = null,
+    noinline create: (stateHandle: SavedStateHandle) -> T
+) = viewModels<T>(ownerProducer, extrasProducer) {
+    DaggerViewModelFactory(this, create)
 }
